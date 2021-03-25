8d6febda87c9e666bccdf2f318b36ce07866d404
katieolson84 <katieolson84@gmail.com>
2021-03-25 14:21:11 -0600

    set up server

---
+++ api/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const Friends = require("./friends/friends-model");
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+server.get("/friends", (req, res) => {
+  Friends.getAll()
+    .then(friends => {
+      res.status(200).json(friends);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.get("/friends/id", (req, res) => {
+  res.end()
+});
+
+server.post("/friends", async (req, res) => {
+  res.json(await Friends.insert(req.body))
+});
+
+server.delete("/friends/:id", (req, res) => {
+  res.end()
+});
+
+server.put("/friends/:id", (req, res) => {
+  res.end()
+});
+
+module.exports = server;
